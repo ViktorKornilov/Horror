0e3a7a14847611f9095fcbc85ebdbb099ee00e58
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-01-04 18:43:39 +0200

    Animation and Slender Model

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
 	public float wanderDistance;
 	public float speed;
 	public Transform target;
+	public Animator animator;
 
 	Rigidbody rb;
 	NavMeshAgent agent;
@@ -15,11 +16,27 @@ public class Enemy : MonoBehaviour
 	{
 		rb = GetComponent<Rigidbody>();
 		agent = GetComponent<NavMeshAgent>();
-		agent.speed = speed;
 	}
 
 	void Update()
 	{
+		var currentSpeed = agent.velocity.magnitude;
+		if (currentSpeed == 0)
+		{
+			animator.Play("Idle");
+		}
+		else if(currentSpeed < 3)
+		{
+			animator.Play("Walk");
+		}
+		else
+		{
+			animator.Play("Run");
+		}
+
+		agent.speed = speed;
+
+
 		var distance = Vector3.Distance(transform.position, target.position);
 		if (distance < viewDistance)
 		{

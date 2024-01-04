da3044b818ff94cf21c2982f6cc5c9c6260bd2dc
Ultimopo <emilismikelionis81@gmail.com>
2024-01-04 17:28:05 +0200

    Update PageManager.cs

--- Assets/PageManager.cs
+++ Assets/PageManager.cs
@@ -2,29 +2,40 @@ using UnityEngine;
 
 public class PageManager : MonoBehaviour
 {
-	public int pages;
-	public Enemy enemy;
+    public int pages;
+    public Enemy enemy;
+    public AudioSource source;
 
-	void OnTriggerEnter(Collider other)
-	{
-		print("page collected!");
-		Destroy(other.gameObject);
+    void OnTriggerEnter(Collider other)
+    {
 
-		pages++;
+        Destroy(other.gameObject);
+        pages++;
 
-		if (pages == 1)
-		{
-			// ai wake up
-			enemy.target = transform;
-		}
-
-		if (pages == 2)
-		{
-			// ai speed 2x
-			enemy.speed *= 2;
-		}
-
-		// 3 more pages
-		// enemy becomes stronger, sounds, second enemy
-	}
+        if (pages == 1)
+        {
+            enemy.target = transform;
+        }
+        if (pages == 2)
+        {
+            enemy.speed *= 2;
+        }
+        if (pages == 3)
+        {
+            enemy.viewDistance *= 2;
+            source.Play();
+        }
+        if (pages == 4)
+        {
+            enemy.speed *= 2;
+            source.Play();
+        }
+        if (pages == 5)
+        {
+            enemy.speed *= 2;
+            enemy.viewDistance *= 100;
+            source.Play();
+        }
+    }
+    
 }
